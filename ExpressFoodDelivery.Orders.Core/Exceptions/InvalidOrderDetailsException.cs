d5ff0442e26943123835efc30824cfa593fa914d
jovanvucetic <vuceticjovan1@gmail.com>
2022-05-23 13:27:33 +0200

    Updated order service code and added unit tests project

--- ExpressFoodDelivery.Orders.Core/Exceptions/InvalidOrderDetailsException.cs
+++ ExpressFoodDelivery.Orders.Core/Exceptions/InvalidOrderDetailsException.cs
@@ -4,5 +4,13 @@ namespace ExpressFoodDelivery.Orders.Core.Exceptions
 {
     public class InvalidOrderDetailsException : Exception
     {
+        public InvalidOrderDetailsException() : base()
+        {
+
+        }
+
+        public InvalidOrderDetailsException(string message) : base(message)
+        {
+        }
     }
 }
--- ExpressFoodDelivery.Orders.Service/Implementation/OrderService.cs
+++ ExpressFoodDelivery.Orders.Service/Implementation/OrderService.cs
@@ -3,7 +3,6 @@ using ExpressFoodDelivery.Orders.Core.Interfaces.Repositories;
 using ExpressFoodDelivery.Orders.Core.Interfaces.Services;
 using ExpressFoodDelivery.Orders.Core.Model;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +10,6 @@ namespace ExpressFoodDelivery.Orders.Service.Implementation
 {
     public class OrderService : IOrderService
     {
-        private const decimal FixedDeliveryFee = 199.99M;
-
         private readonly IDeliveryRepository _deliveryRepository;
         private readonly IPaymentRepository _paymentRepository;
         private readonly IRestaurantRepository _restaurantRepository;
@@ -30,35 +27,53 @@ namespace ExpressFoodDelivery.Orders.Service.Implementation
         public async Task<AcceptedOrderDetails> OrderAsync(Order orderDetails)
         {
             if (orderDetails is null)
-                throw new InvalidOrderDetailsException();
+                throw new InvalidOrderDetailsException("Order details object cannot be null");
 
-            var price = Decimal.Zero;
+            if (orderDetails.DeliveryDetails is null)
+                throw new InvalidOrderDetailsException("Delivery details object cannot be null");
+
+            if (orderDetails.PaymentDetails is null || orderDetails.PaymentDetails.PaymentMethod == PaymentMethod.CreditCard && string.IsNullOrEmpty(orderDetails.PaymentDetails.CardNumber))
+                throw new InvalidOrderDetailsException("Payment details are not valid");
+
+            if(orderDetails.OrderItems is null || orderDetails.OrderItems.Count() == 0)
+            {
+                throw new InvalidOrderDetailsException("Order items must be defined");
+            }
+
             foreach(var item in orderDetails.OrderItems)
+            {
+                if (item is null && item.ItemCount <= 0)
+                    throw new InvalidOrderDetailsException("Order item count must be higher than 0");
+            }
+
+            var price = 0M;
+            foreach (var item in orderDetails.OrderItems)
             {
                 price += item.ItemPrice * item.ItemCount;
             }
 
+            //Adding delivery fee
+            price += 199.99M;
+
             if (orderDetails.PaymentDetails.PaymentMethod == PaymentMethod.CreditCard)
             {
-                var successfullyAuthorized = await _paymentRepository.AuthoriseCreditCardAsync(orderDetails.PaymentDetails.ToCreditCardPayment(price));
-
-                if (!successfullyAuthorized)
+                if (!await _paymentRepository.AuthoriseCreditCardAsync(orderDetails.PaymentDetails.ToCreditCardPayment(price)))
                 {
                     throw new CardAuthorizationException();
                 }
             }
 
-            var restaurantResponse = _restaurantRepository.CreateOrderAsync(orderDetails);
-            var deliveryResponse = _deliveryRepository.CreateDeliveryAsync(orderDetails.DeliveryDetails);
-            var payment = _paymentRepository.ExecutePaymentAsync(orderDetails.PaymentDetails.ToCreditCardPayment(price));
-            var order = _orderRepository.CreateOrderAsync(orderDetails);
+            var restaurantResponseTask = _restaurantRepository.CreateOrderAsync(orderDetails);
+            var deliveryResponseTask = _deliveryRepository.CreateDeliveryAsync(orderDetails.DeliveryDetails);
+            var paymentTask = _paymentRepository.ExecutePaymentAsync(orderDetails.PaymentDetails.ToCreditCardPayment(price));
+            var orderTask = _orderRepository.CreateOrderAsync(orderDetails);
+            
+            await Task.WhenAll(restaurantResponseTask, deliveryResponseTask, paymentTask, orderTask);
 
-            await Task.WhenAll(restaurantResponse, deliveryResponse, payment, order);
+            var now = DateTime.Now;
+            var estimatedDeliveryTime = now.AddMinutes(restaurantResponseTask.Result.EstimatedPreparationTimeInMinutes).AddMinutes(deliveryResponseTask.Result.EstimatedDeliveryTimeInMinutes);
 
-            var orderAcceptedOn = DateTime.Now;
-            var estimatedDeliveryTime = orderAcceptedOn.AddMinutes(restaurantResponse.Result.EstimatedPreparationTimeInMinutes).AddMinutes(deliveryResponse.Result.EstimatedDeliveryTimeInMinutes);
-
-            return new AcceptedOrderDetails(order.Result, orderAcceptedOn, estimatedDeliveryTime);
+            return new AcceptedOrderDetails(orderTask.Result, now, estimatedDeliveryTime);
         }
     }
 }
